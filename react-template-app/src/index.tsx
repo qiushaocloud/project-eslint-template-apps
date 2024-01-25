9a9a22078b23dcde9d0e37a22574c0cc2dca9de4
qiushaocloud <qiushaocloud@126.com>
2024-01-25 14:04:03 +0800

    update

--- react-template-app/src/index.tsx
+++ react-template-app/src/index.tsx
@@ -5,6 +5,8 @@ import {Provider} from 'react-redux';
 import store from '@storets';
 import Router from '@pages/Router';
 import '@styles/app.less';
+import '@styles/less-test-demo.less';
+import '@styles/scss-test-demo.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 // v18 的新方法
